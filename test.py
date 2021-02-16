07df5fa5ff3b06475b6ab75ec3be2102437fe93a
randallle <78888221+randallle@users.noreply.github.com>
2021-02-15 23:00:27 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
 import praw
 
-user1 = praw.Reddit(client_id="Lmq7Hkt7L6giGg",
-                    client_secret="NL9B6JA08bTE6Pbpf91pDBc7L3Ak1Q",
-                    username="",
-                    password="",
-                    user_agent="Account Transfer Program v1 by Rae_Kendell99")
-
-user2 = praw.Reddit(client_id="g9ugkB5qa_l9_w",
-                    client_secret="nl1hZHTioUex3cjzQWspfLLTSMTiiA",
-                    username="",
-                    password="",
-                    user_agent="Account Transfer Program v1 by Randawgy")
-
 numPosts = 0
 display = ""
 
