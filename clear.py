8eb941d5bcc3c506763dc195e37e24bd2659bd9a
randallle <78888221+randallle@users.noreply.github.com>
2021-02-15 23:01:33 -0600

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,11 +1,5 @@
 import praw
 
-user2 = praw.Reddit(client_id="g9ugkB5qa_l9_w",
-                    client_secret="nl1hZHTioUex3cjzQWspfLLTSMTiiA",
-                    username="Randawgy",
-                    password="",
-                    user_agent="Account Transfer Program v1 by Randawgy")
-
 numPosts = 0
 
 for saved in user2.user.me().saved(limit=None):
