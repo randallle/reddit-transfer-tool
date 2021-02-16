247870f633316feabecf5d953675d1978dd43f72
randallle <78888221+randallle@users.noreply.github.com>
2021-02-15 23:01:03 -0600

    Update show.py

--- show.py
+++ show.py
@@ -1,7 +1,1 @@
 import praw
-
-user2 = praw.Reddit(client_id="g9ugkB5qa_l9_w",
-                    client_secret="nl1hZHTioUex3cjzQWspfLLTSMTiiA",
-                    username="Randawgy",
-                    password="",
-                    user_agent="Account Transfer Program v1 by Randawgy")
